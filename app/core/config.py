b074fa4e29287c5c8ed12d245cc489850f8c2404
TranVoAnhTuan <tranvoanhtuan.vietnam@gmail.com>
2026-03-25 20:04:00 +0700

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,10 @@ class Settings:
     API_KEY = os.getenv("API_KEY")
     QDRANT_URL = os.getenv("QDRANT_URL")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
-    QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "pdf_documents")
+    QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
     HUGGINGFACE_ACCESS_TOKEN = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+    SPARSE_MODEL_NAME = os.getenv("SPARSE_MODEL_NAME")
+    DENSE_MODEL_NAME = os.getenv("DENSE_MODEL_NAME")
 
 settings = Settings()
