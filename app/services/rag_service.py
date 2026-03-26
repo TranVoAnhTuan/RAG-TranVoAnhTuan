e91755e39f6206b2b08b24d45251289d7081554c
TranVoAnhTuan <tranvoanhtuan.vietnam@gmail.com>
2026-03-26 15:02:25 +0700

    Borrow GPU to run Reranker, release immediately after scoring

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,54 +1,30 @@
 from qdrant_client import models
-from fastembed import SparseTextEmbedding, TextEmbedding
-from sentence_transformers import SentenceTransformer
-from llmlingua import PromptCompressor
-from FlagEmbedding import FlagReranker
 from app.db.qdrant_db import QdrantDatabase
 from app.core.config import settings
 import asyncio
-# from .qwen_reranker import Qwen3Reranker
 from app.core.model_manager import model_manager
 
 class RAGService:
     def __init__(self):
         self.db = QdrantDatabase()
         self.client = self.db.get_client()
-        self.sparse_model = model_manager.get_sparse_model()
-        self.dense_model = model_manager.get_dense_model()
-
-        # self.sparse_model = SparseTextEmbedding(model_name=settings.SPARSE_MODEL_NAME)
-        # self.dense_model = SentenceTransformer(settings.DENSE_MODEL_NAME,trust_remote_code=True)
-        # self.dense_model = TextEmbedding(model_name="intfloat/multilingual-e5-large")
         
-        # self.llm_lingua = PromptCompressor(
-        #     model_name="microsoft/llmlingua-2-bert-base-multilingual-cased-meetingbank",
-        #     use_llmlingua2=True,
-        # )
-        # self.reranker = FlagReranker("BAAI/bge-reranker-v2-m3", use_fp16=True, devices="cpu")
-        # self.reranker = Qwen3Reranker(use_fp16=True)
-        self.reranker = model_manager.get_reranker_model()
+       # NOTE 1: Only keep the Sparse model as it runs on CPU.
+        # DO NOT initialize Dense Model and Reranker here to avoid occupying VRAM permanently.
+        self.sparse_model = model_manager.get_sparse_model()
 
     async def retrieve_and_rerank(self, query_text: str) -> str:
-        # # 1. Compress prompt
-        # compressed_prompt = self.llm_lingua.compress_prompt(
-        #     query_text,
-        #     rate=0.33,
-        #     force_tokens=['\n', '?']
-        # )
-        # compressed_query = compressed_prompt['compressed_prompt']
-
+        
         # 1. Embed query
         loop = asyncio.get_event_loop()
         def get_embeddings():
-            # Encode trả về numpy array trực tiếp
-            dense = self.dense_model.encode(query_text).tolist() 
+            # NOTE 2: Borrow GPU to create Dense Vector, release immediately after
+            with model_manager.use_dense_model_on_gpu() as gpu_dense_model:
+                dense = gpu_dense_model.encode(query_text).tolist() 
+            
+            # Sparse runs on CPU
             sparse = list(self.sparse_model.embed([query_text]))[0]
             return dense, sparse
-        # loop = asyncio.get_event_loop()
-        # def get_embeddings():
-        #     dense = list(self.dense_model.embed([query_text]))[0].tolist()
-        #     sparse = list(self.sparse_model.embed([query_text]))[0]
-        #     return dense, sparse
 
         dense_query, sparse_query = await loop.run_in_executor(None, get_embeddings)
 
@@ -57,7 +33,7 @@ class RAGService:
             values=sparse_query.values.tolist()
         )
 
-        # 2. Hybrid Search
+        # 2. Hybrid Search (This process calls DB API, VRAM is 100% free at this time)
         hybrid_results = await self.client.query_points(
             collection_name=settings.QDRANT_COLLECTION_NAME,
             prefetch=[
@@ -100,20 +76,21 @@ class RAGService:
         merged_docs = list(merged_docs_dict.values())
 
         # 3. Rerank
-
         def do_rerank():
-            # pairs = [[query_text, doc.payload.get('content', '')] for doc in merged_docs]
             documents = [doc.payload.get('content', '') for doc in merged_docs]
-            # return self.reranker.compute_score(pairs)
-            return self.reranker.rerank(query_text, documents)
+            
+            # NOTE 3: Borrow GPU to run Reranker, release immediately after scoring
+            with model_manager.use_reranker_model() as gpu_reranker:
+                scores= gpu_reranker.rerank(query_text, documents)
+            
+                del gpu_reranker 
+            return scores
 
         jina_results = await loop.run_in_executor(None, do_rerank)
         
-        # Lấy top 5 kết quả tốt nhất. Jina trả về list of dict có dạng: 
-        # {'document': str, 'index': int, 'relevance_score': float}
+        # Get the top 5 best results. 
         top_docs = []
         for result in jina_results[:5]:
-            # Dùng 'index' để map ngược lại mảng merged_docs ban đầu
             original_idx = result['index']
             top_docs.append(merged_docs[original_idx])
 
@@ -122,17 +99,3 @@ class RAGService:
             search_result += f"Result {i+1}: {doc.payload.get('content', '')} citation: (Header 1: {doc.payload.get('Header_1', '')} and Header 2: {doc.payload.get('Header_2', '')} )\n"
             
         return search_result
-        # scores = await loop.run_in_executor(None, do_rerank)
-        
-        # if isinstance(scores, float): scores = [scores]
-
-        # ranked = sorted(zip(merged_docs, scores), key=lambda x: x[1], reverse=True)
-        # top_docs = [doc for doc, score in ranked[:5]]
-
-        # search_result = ""
-        # for i, doc in enumerate(top_docs):
-        #     search_result += f"Result {i+1}: {doc.payload.get('content', '')} citation: (Header 1: {doc.payload.get('Header_1', '')} and Header 2: {doc.payload.get('Header_2', '')} )\n"
-            
-        # return search_result
-
-        
