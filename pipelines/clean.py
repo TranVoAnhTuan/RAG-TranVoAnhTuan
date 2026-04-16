3274363fc2bfdb27f9134177227ef9305ad463d5
TranVoAnhTuan <tranvoanhtuan.vietnam@gmail.com>
2026-04-16 22:48:28 +0700

    update clean and chunk

--- pipelines/clean.py
+++ pipelines/clean.py
@@ -5,6 +5,7 @@ def clean_node(state: IngestionState):
     text = state["raw_text"]
     text = re.sub(r'<!--.*?-->', '', text, flags=re.DOTALL)
     text = re.sub(r'-{4,}', '--', text)
+    text = re.sub(r'_{4,}', '__', text)
     text = re.sub(r'^## (?!\d+\.\d+\b)(.*)', r'### \1', text, flags=re.MULTILINE)
     text = re.sub(r'(?m)^### (.+)\n\s*### (\d+\.(?:\d+)?\s+.*)', r'# \1 - \2',text)
     text = re.sub(r'^###\s*(\d+.*?)$', r'# \1', text, flags=re.MULTILINE)
